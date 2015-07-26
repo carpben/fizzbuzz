88bc6d323aa8397a5611cc0960c2c58ba261338b
carpben <bccarp@gmail.com>
2015-07-26 09:37:49 +0000

    clarified notes

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -3,8 +3,8 @@ import sys
 print "Welcome to the Fizzbuzz Game!" 
 
 # defining n. 
-# first option command line argument. command line can have: 1 argument (no n), 2 arguments (n) or more than 2 arguments (mistake/error)
-# In case of 2 arguments, the second argument is n. otherwise n will be equal to user's input. 
+# first option command line argument. command line can have: 0 argument (no n), 1 arguments (n) or more than 1 arguments (mistake/error)
+# In case of 1 arguments, the second argument is n. otherwise we will ask user to provide input (n). 
 #possible problems: argument or input should be an integer in type string. If that is not the case than we have a problem.  
 # we are instructed to handel error.  
 # it will arise when we try to convert from type string to type integer. 
@@ -17,12 +17,7 @@ while True:
         n=int(n)
         break
     except Exception, e:
-        #while (type(n)==int)==False:
-        n=raw_input ("Please enter a number. We will play Fizzbuzz up to the number you choose.     ")
-            #try:
-        #        n=int(n)
-         #   except Exception, e:
-          #      pass  
+        n=raw_input ("Please enter a number of type integer.      ")
 
 # printing fizzbuzz up to n
 print "Fizz buzz counting up to {}".format (n)
@@ -36,4 +31,3 @@ for num in range (1,n+1):
     else:
         print(num)
         
-
